eb4bc59f3d695eea2d054f1d8848f12ca473a04a
jordanyatckoske <jordanyatckoske@gmail.com>
2020-07-09 22:32:52 -0500

    Inital Commit
    - Bootstrap the project
    - Delete unnecessary files and images
    - create file structure for pages and forms
    - Create register form template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header">        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,9 +16,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
-}
+};
 
 export default App;
