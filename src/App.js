029e86203effed3a4f96a3d881000230dc4e6055
jordanyatckoske <jordanyatckoske@gmail.com>
2020-07-13 21:09:00 -0500

    Simple Page Transitions
    - Add react-transition-group for simple fade transitions between pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
@@ -7,24 +8,32 @@ import Register from "./pages/Register";
 import Confirmation from "./pages/Confirmation";
 import "./styles/global.scss";
 
-const App = () => {
+const App = ({ location }) => {
   return (
     <Layout className="container">
-      <Router>
-        <Switch>
-          <ProtectedRoute exact path="/">
-            <Home />
-          </ProtectedRoute>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <ProtectedRoute path="/confirmation">
-            <Confirmation />
-          </ProtectedRoute>
-        </Switch>
-      </Router>
+      <TransitionGroup className="transition-group">
+        <CSSTransition
+          key={location.key}
+          timeout={{ enter: 300, exit: 300 }}
+          classNames={"fade"}
+        >
+          <div className="section">
+            <Switch location={location}>
+              <ProtectedRoute exact path="/">
+                <Home />
+              </ProtectedRoute>
+              <Route path="/register">
+                <Register />
+              </Route>
+              <ProtectedRoute path="/confirmation">
+                <Confirmation />
+              </ProtectedRoute>
+            </Switch>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
     </Layout>
   );
 };
 
-export default App;
+export default withRouter(App);
